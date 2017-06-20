dd2c758110fd983ec2acf4e47cf5280aee8726e0
Johannes Wilm <mail@johanneswilm.org>
2017-06-20 04:01:26 +0200

    release 3.2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='fiduswriter-books',
-    version='3.2.0-rc.1',
+    version='3.2.0',
     packages=find_packages(),
     include_package_data=True,
     license='AGPL License',
