607c963d6b51aa00fde2667a4ff525347c4ed93d
Johannes Wilm <mail@johanneswilm.org>
2020-02-23 23:37:43 +0100

    basic book test

--- fiduswriter/book/tests/test_book.py
+++ fiduswriter/book/tests/test_book.py
@@ -1,51 +1,146 @@
-import unittest
-import os
-import multiprocessing
 import time
-import shutil
-import subprocess
-import urllib.request
+import os
 
-from pathlib import Path
-from tempfile import mkdtemp
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from testing.testcases import LiveTornadoTestCase
+from testing.selenium_helper import SeleniumHelper
+from selenium.common.exceptions import StaleElementReferenceException
 
+from django.core import mail
 from django.conf import settings
+from django.contrib.auth.models import User
 
 
-class ManageTest(unittest.TestCase):
+class BookTest(LiveTornadoTestCase, SeleniumHelper):
+    fixtures = [
+        'initial_documenttemplates.json',
+        'initial_styles.json',
+    ]
 
-    def test_startserver(self):
-        os.chdir(settings.PROJECT_PATH)
-        temp_dir = mkdtemp()
-        p1 = multiprocessing.Process(
-            target=self.start_fidus,
-            args=(temp_dir,)
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.base_url = cls.live_server_url
+        driver_data = cls.get_drivers(1)
+        cls.driver = driver_data["drivers"][0]
+        cls.client = driver_data["clients"][0]
+        cls.driver.implicitly_wait(driver_data["wait_time"])
+        cls.wait_time = driver_data["wait_time"]
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+        super().tearDownClass()
+
+    def setUp(self):
+        self.verificationErrors = []
+        self.accept_next_alert = True
+        self.user = self.create_user(
+            username='Yeti',
+            email='yeti@snowman.com',
+            passtext='otter1'
         )
-        p1.start()
-        time.sleep(1)
-        sql_file = Path(os.path.join(temp_dir, 'fiduswriter.sql'))
-        assert sql_file.exists()
-        # Get page during transpile to see if we get setup page
-        page = urllib.request.urlopen(
-            'http://localhost:7000/'
-        ).read().decode('utf-8')
-        assert "Fidus Writer is currently being updated." in page
-        # We remove the project dir. The process should then finish as well.
-        shutil.rmtree(temp_dir)
-        p1.join()
+        self.login_user(self.user, self.driver, self.client)
 
-    def start_fidus(self, temp_dir):
-        os.environ.pop("PROJECT_PATH")
-        if os.getenv("COVERAGE_RCFILE"):
-            os.environ['COVERAGE_PROCESS_START'] = '.coveragerc'
-        subprocess.call(
-            [
-                "./manage.py",
-                "--pythonpath",
-                temp_dir,
-                "runserver",
-                "7000"
-            ],
-            stdout=open(os.devnull, 'w'),
-            stderr=subprocess.STDOUT
+    def tearDown(self):
+        self.assertEqual([], self.verificationErrors)
+
+    def assertInfoAlert(self, message):
+        i = 0
+        message_found = False
+        while(i < 100):
+            i = i + 1
+            try:
+                if self.driver.find_element(
+                    By.CSS_SELECTOR,
+                    "body #alerts-outer-wrapper .alerts-info"
+                ).text == message:
+                    message_found = True
+                    break
+                else:
+                    time.sleep(0.1)
+                    continue
+            except StaleElementReferenceException:
+                time.sleep(0.1)
+                continue
+        self.assertTrue(message_found)
+
+    def test_books(self):
+        self.driver.get(self.base_url + "/")
+        # Create chapter one doc
+        WebDriverWait(self.driver, self.wait_time).until(
+            EC.element_to_be_clickable(
+                (
+                    By.CSS_SELECTOR,
+                    ".new_document button"
+                )
+            )
+        ).click()
+        WebDriverWait(self.driver, self.wait_time).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'editor-toolbar'))
         )
+        self.driver.find_element(By.CSS_SELECTOR, ".article-title").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".article-title").send_keys(
+            "Chapter 1"
+        )
+        self.driver.find_element(
+            By.ID,
+            "close-document-top"
+        ).click()
+        # Create chapter two doc
+        WebDriverWait(self.driver, self.wait_time).until(
+            EC.element_to_be_clickable(
+                (
+                    By.CSS_SELECTOR,
+                    ".new_document button"
+                )
+            )
+        ).click()
+        WebDriverWait(self.driver, self.wait_time).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'editor-toolbar'))
+        )
+        self.driver.find_element(By.CSS_SELECTOR, ".article-title").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".article-title").send_keys(
+            "Chapter 2"
+        )
+        self.driver.find_element(
+            By.ID,
+            "close-document-top"
+        ).click()
+        # Create chapter three doc
+        WebDriverWait(self.driver, self.wait_time).until(
+            EC.element_to_be_clickable(
+                (
+                    By.CSS_SELECTOR,
+                    ".new_document button"
+                )
+            )
+        ).click()
+        WebDriverWait(self.driver, self.wait_time).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'editor-toolbar'))
+        )
+        self.driver.find_element(By.CSS_SELECTOR, ".article-title").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".article-title").send_keys(
+            "Chapter 3"
+        )
+        self.driver.find_element(
+            By.ID,
+            "close-document-top"
+        ).click()
+        # Go to book section
+        WebDriverWait(self.driver, self.wait_time).until(
+            EC.presence_of_element_located(
+                (By.CSS_SELECTOR, 'a[href="/book/"]')
+            )
+        ).click()
+        # Create a new book
+        self.driver.find_element(
+            By.CSS_SELECTOR,
+            'button[title="Create new book"]'
+        ).click()
+
+
