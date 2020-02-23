d8aec5ee80a024aecf47c0feedf2a9b420158444
Johannes Wilm <mail@johanneswilm.org>
2020-02-23 20:45:54 +0100

    first trial travis test

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='fiduswriter-books',
-    version='3.7.5',
+    version='3.8.0-dev',
     packages=find_namespace_packages(),
     include_package_data=True,
     license='AGPL License',
