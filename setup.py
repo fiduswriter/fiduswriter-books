dc2017fccf6f8ea4af57ae5b315eebf6c3589c0a
Johannes Wilm <mail@johanneswilm.org>
2020-02-27 12:42:59 +0100

    update packaging

--- setup.py
+++ setup.py
@@ -1,5 +1,27 @@
 import os
+from glob import glob
 from setuptools import find_namespace_packages, setup
+from setuptools.command.build_py import build_py as _build_py
+
+# From https://github.com/pypa/setuptools/pull/1574
+class build_py(_build_py):
+    def find_package_modules(self, package, package_dir):
+        modules = super().find_package_modules(package, package_dir)
+        patterns = self._get_platform_patterns(
+            self.exclude_package_data,
+            package,
+            package_dir,
+        )
+
+        excluded_module_files = []
+        for pattern in patterns:
+            excluded_module_files.extend(glob(pattern))
+
+        for f in excluded_module_files:
+            for module in modules:
+                if module[2] == f:
+                    modules.remove(module)
+        return modules
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
@@ -10,9 +32,15 @@ setup(
     name='fiduswriter-books',
     version='3.8.0-dev',
     packages=find_namespace_packages(),
+    exclude_package_data={
+        "": ["configuration.py", "django-admin.py", "build/*"]
+    },
     include_package_data=True,
     license='AGPL License',
-    description='A Fidus Writer plugin to allow creation of books and article collections',
+    description=(
+        'A Fidus Writer plugin to allow creation of books '
+        'and article collections'
+    ),
     long_description=README,
     url='https://www.github.org/fiduswriter/fiduswriter-books',
     author='Johannes Wilm',
@@ -29,4 +57,5 @@ setup(
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
+    cmdclass={'build_py': build_py}
 )
