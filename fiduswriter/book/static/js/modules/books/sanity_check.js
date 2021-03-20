ceac320c17fa0d551fc620560fcd2325dd06a077
Johannes Wilm <mail@johanneswilm.org>
2021-03-20 06:32:16 +0100

    sanity check: check for missing targets

--- fiduswriter/book/static/js/modules/books/sanity_check.js
+++ fiduswriter/book/static/js/modules/books/sanity_check.js
@@ -3,18 +3,27 @@ import {escapeText} from "../common"
 import {getMissingChapterData} from "./exporter/tools"
 
 
+const labelChapter = chapter => `(${gettext('Chapter')} ${chapter.number}, ${escapeText(chapter.title || '')})`
+
 function findContentIssues(node, chapter, doc, messages) {
-    if (node.attrs?.track?.length || node.marks?.find(mark => mark.type === 'deletion' || (mark.type === 'insertion' && mark.attrs.approved === false))) {
-        messages.warnings.push(`${gettext('Unresolved tracked changes')} (${gettext('Chapter')} ${chapter.number}, ${escapeText(chapter.title)})`)
+    if (node.attrs?.track?.length) {
+        messages.warnings.push(`${gettext('Unresolved tracked changes')} ${labelChapter(chapter)}`)
     }
-    if (node.marks?.find(mark => {
-        if (mark.type !== 'comment') {
-            return false
-        }
-        return doc.comments[parseInt(mark.attrs.id)].resolved === false
-    })) {
-        messages.warnings.push(`${gettext('Unresolved comments')} (${gettext('Chapter')} ${chapter.number}, ${escapeText(chapter.title)})`)
+    if (node.marks) {
+        node.marks.forEach(mark => {
+            if (mark.type == 'link' && mark.attrs.href.charAt(0) === '#' && !mark.attrs.title) {
+                messages.warnings.push(`${gettext('Internal links without target')} ${labelChapter(chapter)}`)
+            } else if (mark.type == 'comment' && doc.comments[parseInt(mark.attrs.id)].resolved === false) {
+                messages.warnings.push(`${gettext('Unresolved comments')} ${labelChapter(chapter)}`)
+            } else if (mark.type === 'deletion' || (mark.type === 'insertion' && mark.attrs.approved === false)) {
+                messages.warnings.push(`${gettext('Unresolved tracked changes')} ${labelChapter(chapter)}`)
+            }
+        })
     }
+    if (node.type === 'cross_reference' && !node.attrs.title) {
+        messages.warnings.push(`${gettext('Cross references without targets')} ${labelChapter(chapter)}`)
+    }
+
     if (node.content) {
         node.content.forEach(subNode => findContentIssues(subNode, chapter, doc, messages))
     }
@@ -37,9 +46,12 @@ export const bookSanityCheck = (book, documentList, schema) => {
             }
             book.chapters.forEach(chapter => {
                 const doc = documentList.find(doc => doc.id === chapter.text)
-                if (!doc.rawContent) {
-                    messages.errors.push(`${gettext('No access')} (${gettext('Chapter')} ${chapter.number}, ${escapeText(chapter.title)})`)
+                if (!doc || !doc.rawContent) {
+                    messages.errors.push(`${gettext('No access')} ${labelChapter(chapter)}`)
                     return
+                }
+                if (!doc.title || !doc.title.length) {
+                    messages.warnings.push(`${gettext('No chapter title')} ${labelChapter(chapter)}`)
                 }
                 findContentIssues(doc.rawContent, chapter, doc, messages)
             })
@@ -47,7 +59,13 @@ export const bookSanityCheck = (book, documentList, schema) => {
             const warnings = Array.from(new Set(messages.warnings))
             const errors = Array.from(new Set(messages.errors))
             if (!warnings.length && !errors.length) {
-                return `<p>${gettext('No issues were found.')}</p>`
+                return `<p>${gettext('No issues were found:')}</p>
+                    <p>${gettext('The book contains chapters.')}</p>
+                    <p>${gettext('Each chapter has a title.')}</p>
+                    <p>${gettext('There are no unresolved tracked changes.')}</p>
+                    <p>${gettext('There are no unresolved comments.')}</p>
+                    <p>${gettext('All cross references have working targets.')}</p>
+                    <p>${gettext('All internal links have working targets.')}</p>`
             }
             return `<ul class="warninglist">
                     ${warnings.map(warning => `<li>${warning}</li>`).join('')}
