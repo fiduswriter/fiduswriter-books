9f5409f3a95d5cdf8990438e36a252107fdb8387
Johannes Wilm <mail@johanneswilm.org>
2023-04-17 12:45:00 +0200

    move info setup.py => pyproject.toml

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 FidusWriter-Books
-=================
+====
 
 FidusWriter-Books is a Fidus Writer plugin to allow for the composition of books
 and article collections, using individual documents as chapters.
@@ -10,15 +10,19 @@ Installation
 
 1) Install Fidus Writer like this:
 
+```shell
     pip install fiduswriter[books]
+```
 
 2) Add "book" to your INSTALLED_APPS setting in the
-   configuration.py file like this::
+   configuration.py file like this:
 
+```python
     INSTALLED_APPS += (
         ...
         'book',
     )
+```
 
 3) Run ``fiduswriter setup`` to create the needed database tables and to create the needed JavaScript files.
 
--- setup.py
+++ setup.py
@@ -24,40 +24,13 @@ class build_py(_build_py):
                     modules.remove(module)
         return modules
 
-
-with open(os.path.join(os.path.dirname(__file__), "README.rst")) as readme:
-    README = readme.read()
-
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name="fiduswriter-books",
-    version="3.11.2",
     packages=find_namespace_packages(),
     exclude_package_data={
         "": ["configuration.py", "django-admin.py", "build/*"]
     },
     include_package_data=True,
-    license="AGPL License",
-    description=(
-        "A Fidus Writer plugin to allow creation of books "
-        "and article collections"
-    ),
-    long_description=README,
-    url="https://www.github.org/fiduswriter/fiduswriter-books",
-    author="Johannes Wilm",
-    author_email="johannes@fiduswriter.org",
-    classifiers=[
-        "Environment :: Web Environment",
-        "Framework :: Django",
-        "Framework :: Django :: 4.1",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU Affero General Public License v3",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-    ],
     cmdclass={"build_py": build_py},
 )
